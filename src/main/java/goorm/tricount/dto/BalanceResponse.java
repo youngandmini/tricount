317e4acf89f5dec80afa76910f14a1743bb28146
youngmin <dudals9901@naver.com>
2023-11-15 01:06:11 +0900

    feat: BalanceResponse 정산 결과 DTO 내부 구현중

--- src/main/java/goorm/tricount/dto/BalanceResponse.java
+++ src/main/java/goorm/tricount/dto/BalanceResponse.java
@@ -1,12 +1,24 @@
 package goorm.tricount.dto;
 
 
+import goorm.tricount.domain.User;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 public class BalanceResponse {
 
+    private Long senderUserId;
+    private String senderUsername;
+    private Long receiverUserId;
+    private String receiverUsername;
+    private BigDecimal sendAmount;
+
+    public static BalanceResponse of(User sender, User receiver, BigDecimal sendAmount) {
+        return null;
+    }
 }
